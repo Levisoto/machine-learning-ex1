997dfcbe71c7e8c089cc6bbd471d7a44780a80c8
Levisoto <levisoto.337@gmail.com>
2017-12-31 09:03:33 -0500

    I completed the gradient descent with tree, maybe I need to change that

--- ex1-haskell/app/Main.hs
+++ ex1-haskell/app/Main.hs
@@ -12,7 +12,14 @@ main = do
  -- putStrLn "quit the program? y/n"
  -- ans <- getLine
  datas <- readFile "ex1data1.txt"
- functionY (takeFrom datas)
+ -- functionY (takeFrom datas)
+ let (y,x) = unzip $ takeFrom' datas
+     input = toValues x y [0,0] 0.01
+     (Cons j value _)= takeLinReg 15000 $ Single input
+     -- [b0,b1] = theta value
+  in writeFile "jvalues.txt" $ show (theta value)
+  -- putStrLn $ (show.funCost) input
+  -- in functionY (takeFrom datas) (\ x -> b0 + b1*x)
  return()
  -- if ans /= "y" then do
  --   putStrLn "not quitting"
--- ex1-haskell/src/Lib.hs
+++ ex1-haskell/src/Lib.hs
@@ -1,60 +1,91 @@
 module Lib where
 
-import Statistics.LinearRegression
-import qualified Data.Vector.Unboxed as U
+-- import Statistics.LinearRegression
+-- import qualified Data.Vector.Unboxed as U
+import Data.Monoid
 import Data.Decimal
 import Data.Matrix
 import Graphics.EasyPlot
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
---xs :: [Double]
---xs = [25.47,67.80,20.65,22.50,30.40,55.30,22.74] ++
---         [29.11,31.15,40.97,58.89,64.85,74.89,96.14] ++
---         [65.37,80.23,95.80,92.71,98.16,89.77]
 
---ys :: [Double]
---ys = [390.50,632.70,449.36,501.81,621.72,684.62,685.53] ++
---         [701.76,868.71,995.69,893.50,1240.48,1393.55] ++
---         [1826.70,2145.32,2295.27,2522.79,2931.69] ++
---         [2836.20,2456.30]
+data LinReg m a = Single a 
+                | Cons m a (LinReg m a)
+                deriving (Show, Eq)
 
----- Values from a list
-----
---varIndep  = U.fromList xs
---varDepend = U.fromList ys
+data Values = Values {xval :: Matrix Double , yval :: Matrix Double,  
+  theta :: Matrix Double , alpha :: Double}
+  deriving (Show,Eq)
 
----- computing theta0 and theta1 : Y = t0 + b1.X
 
---(b0, b1) = linearRegression varIndep varDepend
 
---b0n = realFracToDecimal 3 b0
---b1n = realFracToDecimal 3 b1
+tag::(Monoid m) => LinReg m (Values) -> m
+tag (Single _) = mempty
+tag (Cons m _ _) = m
 
---points = zip xs ys
+compute :: LinReg [Double] Values -> LinReg [Double] Values
+compute b@(Single a) = Cons (tag b) (newV a) b
+compute b@(Cons m a val) = Cons ([funCost a]<>(tag val)) (newV a) b
 
---f x = b0 + b1*x
---g x = ((*b0).sin.(/b1)) x
+newV :: Values -> Values
+newV a@(Values xval yval theta alpha) = Values xval yval (funGrad a) alpha
 
-functionY datas= do 
+takeLinReg:: Int -> LinReg [Double] Values -> LinReg [Double] Values 
+takeLinReg 0 val = val
+takeLinReg n val  
+  | n>=0 = takeLinReg (n - 1) (compute val)
+  | otherwise = val
+
+functionY :: [(Double,Double)] -> (Double-> Double) -> [(Double,Double,Double)] -> IO Bool
+functionY datas g data3d = do 
   plot X11 $ [
                     -- Function2D
                     -- [Title "Linear Regression", Color Red]
                     -- [Range 0 25] (g)
-                     Data2D
-                    [Title "Data", Color Blue]
-                    [Range 0 25] datas
-                    -- , Function2D
-                    -- [Title "Linear Regression", Color Red]
-                    -- [Range 0 25] (f)
+                    -- , Data2D
+                    -- [Title "Data", Color Blue]
+                    -- [Range 0 25] datas
+                    -- , 
+                    Data3D
+                    [Title "theta vs J", Color Green]
+                    [RangeY 0 25] data3d
                  ]
 
 
 
 takeFrom :: String -> [(Double,Double)]
-takeFrom = map divByComa.lines
+takeFrom = map (\(x:y:_)->(x,y)).map divByComa.lines
 
-divByComa :: String -> (Double, Double)
-divByComa val = ((read.ins) val, (read.reverse.ins.reverse) val)
-  where 
-    ins = takeWhile (/= ',')
+takeFrom' :: String -> [(Double,[Double])]
+takeFrom' = map (\(x:xs)-> (x,reverse xs)).map (reverse.divByComa).lines
+
+divByComa :: String -> [Double]
+divByComa = map read.words.map rep
+  where
+    rep ',' = ' '
+    rep c = c
+
+toValues:: [[Double]] -> [Double] -> [Double] -> Double -> Values
+toValues x y theta alpha = Values x' y' theta' alpha
+  where
+    m      = length x
+    x'     = ((fromLists.take m.repeat) [1]) <|> fromLists x
+    y'     = fromList m 1 y
+    theta' = fromList (length theta) 1 theta
+
+funGrad :: Values -> Matrix Double
+funGrad (Values x y theta alpha) = final
+    where
+      m      = nrows x
+      sol    = elementwise (\h g -> h-g)(multStd2 x theta) y
+      sol'   = multStd2 (transpose x) sol
+      final = elementwise (\h g -> h-g) theta (scaleMatrix (alpha/(fromIntegral m)) sol')
+
+
+funCost:: Values -> Double
+funCost (Values x y theta alpha) = (0.5/(fromIntegral m))*(getSum $ foldMap (\h->Sum h) sol')
+    where
+      m      = nrows x
+      sol    = elementwise (\h g -> h-g)(multStd2 x theta) y
+      sol'   = fmap (\x -> x^2) sol
